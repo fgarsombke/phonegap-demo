da1b2573fb9409e6c16e969ba7a51982d44f6603
franz <franz@franz-ubuntu.(none)>
2013-04-02 21:25:50 -0600

    1st

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
     showAlert: function (message, title) {
@@ -19,10 +13,17 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
     initialize: function() {
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
